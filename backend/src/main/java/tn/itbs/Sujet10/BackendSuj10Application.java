a80ccc422e4c0e058e7279eade86f4b4e4e239c4
Nouha <berguiganouha62@gmail.com>
2026-05-06 02:00:59 +0200

    init spring boot backend

---
+++ backend/src/main/java/tn/itbs/Sujet10/BackendSuj10Application.java
@@ -0,0 +1,13 @@
+package tn.itbs.Sujet10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSuj10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSuj10Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tn/itbs/Sujet10/BackendSuj10ApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.itbs.Sujet10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSuj10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
