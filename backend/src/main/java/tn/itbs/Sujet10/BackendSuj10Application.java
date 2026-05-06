c4ccc682d7e17a64acbfcf67f32ae9233c6b84c9
Nouha <berguiganouha62@gmail.com>
2026-05-06 11:43:54 +0200

    j'ai ajoutée swagger

--- backend/src/main/java/tn/itbs/Sujet10/BackendSuj10Application.java
+++ backend/src/main/java/tn/itbs/Sujet10/BackendSuj10Application.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendSuj10Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendSuj10Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSuj10Application.class, args);
+    }
 }
